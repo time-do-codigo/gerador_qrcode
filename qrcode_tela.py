dc8bad05f7c6f69d218ff14e44c89f6bd4a19cf4
Isabel-tech <isabelmeirabarreto@gmail.com>
2024-10-11 13:50:27 -0300

    terminei a minha parte

--- qrcode_tela.py
+++ qrcode_tela.py
@@ -1,6 +1,6 @@
 from tkinter import *
-from PIL import Image, ImageTk
 import tkinter as tk
+from PIL import Image, ImageTk
 
 #criação da janela
 
@@ -15,11 +15,11 @@ frame.place(width=600, height=500)
 titulo = Label(frame, text='Gerador QR Code', font=('Consolas', 24), bg='#3b3b3b', fg='#feffff')
 titulo.pack(pady = 10)
 
-texto1 = Label(frame, text='Insira o seu link para gerar', font=('bold', 12), bg='#3b3b3b', fg='#feffff')
+texto1 = Label(frame, text='Insira o seu link para gerar', font=('Helvetica', 12), bg='#3b3b3b', fg='#feffff')
 texto1.place(x=200, y=200)
 
 texto_link = Entry(frame, text='', font=('bold', 12))
-texto_link.place(x=80, y=230, width=400, height=25)
+texto_link.place(x=100, y=230, width=400, height=28)
 
 #imagens janela principal
 
@@ -35,13 +35,17 @@ logo1 = Image.open('imagens/link.png')
 logo1 = logo1.resize((50,50), Image.LANCZOS)
 logo1 = ImageTk.PhotoImage(logo1)
 label_logo1= Label(janela, image=logo1, compound=LEFT, anchor='nw',bg='#3b3b3b')
-label_logo1.place(x=20, y=217)
+label_logo1.place(x=40, y=220)
 
 #imagem botao
 logo2 = Image.open('imagens/botao_criar.png')
-logo2 = logo2.resize((41,42), Image.LANCZOS)
+logo2 = logo2.resize((40,40), Image.LANCZOS)
 logo2 = ImageTk.PhotoImage(logo2)
 label_logo2= Button(janela, command='', image=logo2, compound=LEFT, anchor='nw',bg='#3b3b3b', bd=0, activebackground="#FFFFFF")
-label_logo2.place(x=495, y=220)
+label_logo2.place(x=507, y=222)
 
-janela.mainloop()
+janela.mainloop() 
+
+
+
+
